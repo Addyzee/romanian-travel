0bb900dfad45573ed2f4e1c17ff7ac3948638a9e
Andrew Kimani <andrewkamaukim@gmail.com>
2024-11-05 09:59:45 +0300

    code update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-class Node:
-    def __init__(self, name):
-        self.name = name
-        self.neighbours = {}
-        self.visited = False
+from typing import List, Dict, Optional
 
+class Node:
+    def __init__(self, name: str):
+        self.name = name
+        self.neighbours: Dict['Node', int] = {}
+        
     def __repr__(self):
         neighbours_str = {
             neighbour.name: distance for neighbour, distance in self.neighbours.items()
@@ -13,106 +14,74 @@ class Node:
     def __str__(self):
         return self.__repr__()
 
+def load_graph(filename: str) -> Dict[str, Node]:
+    """Parse input file and return a dictionary of city nodes with neighbors and distances."""
+    cities_dict = {}
+    
+    with open(filename, "r") as file:
+        for line in file:
+            city, neighbour, distance = line.split(",")
+            distance = int(distance.strip())
+            
+            if city not in cities_dict:
+                cities_dict[city] = Node(city)
+            if neighbour not in cities_dict:
+                cities_dict[neighbour] = Node(neighbour)
+            
+            # Update neighbors in both directions
+            cities_dict[city].neighbours[cities_dict[neighbour]] = distance
+            cities_dict[neighbour].neighbours[cities_dict[city]] = distance
 
-with open("input.txt", "r") as file:
-    nodes = file.readlines()
+    return cities_dict
 
+def dfs(node_start: Node, node_end: Node, visited: Optional[set] = None, path: Optional[List[Node]] = None) -> Optional[List[Node]]:
+    """
+    Perform DFS to find a path from node_start to node_end.
 
-cities_dict = {}
+    Args:
+        node_start (Node): The starting node.
+        node_end (Node): The destination node.
+        visited (set): Set of visited nodes to avoid cycles.
+        path (list): Current path of nodes.
 
-for node in nodes:
-    city, neighbour, distance = node.split(",")
-    distance = int(distance.strip())
-
-    if city not in cities_dict:
-        cities_dict[city] = {}
-    if neighbour not in cities_dict:
-        cities_dict[neighbour] = {}
-
-    cities_dict[city][neighbour] = distance
-    cities_dict[neighbour][city] = distance
-
-"""
-cities_dict = 
-{
-    'Arad': 
-        {'Zerind': 75,
-         'Timisoara': 118,
-         'Sibiu': 140}...
-}
-"""
-
-
-cities_obj = {}
-
-for city in cities_dict:
-    cities_obj[city] = Node(city)
-
-"""
-cities_obj =
-{
-    'Arad': Node('Arad'),
-    'Zerind': Node('Zerind'),
-    'Timisoara': Node('Timisoara'),
-    'Sibiu': Node('Sibiu'),
-    ...
-}
-"""
-
-
-# adding neighbours and distances to the nodes
-for city, neighbours in cities_dict.items():
-    for neighbour, distance in neighbours.items():
-        """
-        eg
-        city = 'Arad', neighbour = 'Zerind', distance = 75
-        # cities_obj[city] -> Node('Arad').neighbours[Node('Zerind')] = 75
-        """
-        cities_obj[city].neighbours[cities_obj[neighbour]] = distance
-
-'''
-Arad -> {'Zerind': 75, 'Timisoara': 118, 'Sibiu': 140}
-Zerind -> {'Arad': 75, 'Oradea': 71}
-'''
-
-
-
-def dfs(node_start, node_end, path=None):
+    Returns:
+        list: A list of nodes representing the path, or None if no path found.
+    """
+    if visited is None:
+        visited = set()
     if path is None:
         path = []
 
-    # Add current node to path
-    path = path + [node_start]
+    # Add current node to path and mark as visited
+    path.append(node_start)
+    visited.add(node_start)
 
-    # Destination reached
+    # Check if destination is reached
     if node_start == node_end:
         return path
 
-    node_start.visited = True
-
-    # Visit each unvisited neighbour recursively
+    # Explore each unvisited neighbour
     for neighbour in node_start.neighbours:
-        if not neighbour.visited:
-            new_path = dfs(neighbour, node_end, path)
-            if new_path:  # Path found
-                return new_path
+        if neighbour not in visited:
+            result = dfs(neighbour, node_end, visited, path)
+            if result:  # Path found
+                return result
 
-    # If no path found, return None
+    # Backtrack if no path found
+    path.pop()
     return None
 
 
-# Reset the visited attr
-def reset_visited(cities_obj):
-    for node in cities_obj.values():
-        node.visited = False
-
-
-start_city = "Arad"
-end_city = "Bucharest"
-path_result = dfs(cities_obj[start_city], cities_obj[end_city])
-
-reset_visited(cities_obj)
-
-print("Path from", start_city, "to", end_city, ":", path_result)
-
-
+# Main
+if __name__ == "__main__":
+    cities_obj = load_graph("input.txt")
+    
+    start_city = "Arad"
+    end_city = "Bucharest"
+    
+    path_result = dfs(cities_obj[start_city], cities_obj[end_city])
+    
+    if path_result:
+        print("Path from", start_city, "to", end_city, ":", " -> ".join([node.name for node in path_result]))
+    else:
+        print("No path found from", start_city, "to", end_city)
